c6697cf2e429919b6d67e82124a8e84384948a63
KostyaSkorik <kostya.skorik.2016@mail.ru>
2024-09-02 21:35:12 +0300

    JRTB-2: Add telegram bot to the project

--- src/main/java/com/github/javarushcommunity/javarush_telegrambot/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/javarush_telegrambot/JavarushTelegramBotApplication.java
@@ -2,8 +2,7 @@ package com.github.javarushcommunity.javarush_telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
@@ -11,10 +10,7 @@ public class JavarushTelegramBotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavarushTelegramBotApplication.class, args);
 	}
-	@PostMapping("/test-request")
-	public ResponseEntity<String> testPostRequest(){
-		return ResponseEntity.ok("POST request successful");
-	}
+
 
 
 }
