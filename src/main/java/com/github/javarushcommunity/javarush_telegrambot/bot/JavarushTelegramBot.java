33191b88665681b7b2e8f9bd6b9ced815e779d73
KostyaSkorik <kostya.skorik.2016@mail.ru>
2024-09-02 22:14:09 +0300

    add bot

---
+++ src/main/java/com/github/javarushcommunity/javarush_telegrambot/bot/JavarushTelegramBot.java
@@ -0,0 +1,47 @@
+package com.github.javarushcommunity.javarush_telegrambot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class JavarushTelegramBot extends TelegramLongPollingBot {
+
+    @Value(value = "${bot.username}")
+    private String botUsername;
+    @Value(value = "${bot.token}")
+    private String botToken;
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+
+            SendMessage sm = new SendMessage();
+            sm.setChatId(chatId);
+            sm.setText(message);
+
+            try {
+                execute(sm);
+            } catch (TelegramApiException e) {
+                //todo add logging to the project.
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
